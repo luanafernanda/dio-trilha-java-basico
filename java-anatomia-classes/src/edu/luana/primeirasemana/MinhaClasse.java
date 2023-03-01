af2b74e18f447fb8d561935458efd5a7f20f551f
luanafernanda <lfsg.22@outlook.com>
2023-03-01 14:56:18 -0300

    adicionado pasta primeira semana

---
+++ java-anatomia-classes/src/edu/luana/primeirasemana/MinhaClasse.java
@@ -0,0 +1,21 @@
+package edu.luana.primeirasemana;
+public class MinhaClasse {
+
+  public static void main (String []args) {
+    String primeiroNome = "Luana";
+    String segundoNome = "Fernanda";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+  }
+
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+  
+  }
+
+
+}
